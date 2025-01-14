5e797401202824c75f5e0aa122b67e69204847f4
yorkdick <yorkdick@163.com>
2025-01-14 10:50:56 +0800

    update

--- app/gira-frontend/src/utils/withSuspense.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Suspense } from 'react';
-import { Spin } from 'antd';
-
-export const withSuspense = (Component: React.ComponentType) => (
-  <Suspense fallback={<Spin size="large" />}>
-    <Component />
-  </Suspense>
-); 
